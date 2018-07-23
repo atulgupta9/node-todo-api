5116b6bf999ef380025579b4c05f106f105c7260
Atul Gupta (UST <U65980@ust-global.com>
2018-07-23 17:55:01 +0530

    Test the DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-//mongoose.connect("mongodb://localhost:27017/TodoApp");
-mongoose.connect("mongodb://test:test123@ds145981.mlab.com:45981/to-do-api");
+mongoose.connect("mongodb://localhost:27017/TodoApp");
+//mongoose.connect("mongodb://test:test123@ds145981.mlab.com:45981/to-do-api");
 module.exports={mongoose};
