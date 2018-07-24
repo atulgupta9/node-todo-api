f044359a4f644448663e440be2ddb7e49f2b91b6
Atul Gupta (UST <U65980@ust-global.com>
2018-07-24 15:35:41 +0530

    Fixed Server

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const {ObjectID} = require("mongodb");
 var {mongoose} = require("./db/mongoose");
 var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
-var
+
 var app = express();
 
 
