93b4c02aa09186fbcc365cf6ccb03ff514fd5a8b
Atul Gupta (UST <U65980@ust-global.com>
2018-07-25 15:36:12 +0530

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require("./config/config.js");
 
+
 const _ = require("lodash");
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -8,6 +9,7 @@ const {ObjectID} = require("mongodb");
 var {mongoose} = require("./db/mongoose");
 var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
+var {authenticate} = require("./middleware/authenticate");
 
 var app = express();
 
@@ -96,6 +98,12 @@ app.post("/users", (req,res) => {
   }).catch((err) => {
     res.status(400).send(err);
   });
+});
+
+
+
+app.get("/users/me",authenticate, (req,res) => {
+  res.send(req.user);
 });
 
 app.listen(process.env.PORT, ()=>{
