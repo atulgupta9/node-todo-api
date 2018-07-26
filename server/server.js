13c149620d6d8295b0e93ef0e70ff18ceaebe7ec
Atul Gupta (UST <U65980@ust-global.com>
2018-07-26 14:45:50 +0530

    Add POST /users/login routes

--- server/server.js
+++ server/server.js
@@ -106,6 +106,18 @@ app.get("/users/me",authenticate, (req,res) => {
   res.send(req.user);
 });
 
+//POST /users/login {email,password}
+app.post("/users/login", (req,res) => {
+  var body = _.pick(req.body,["email","password"]);
+  User.findByCredentials(body.email,body.password).then((user) =>{
+    return user.generateAuthToken().then((token) =>{
+      res.header("x-auth",token).send(user);
+    });
+  }).catch((er) => {
+    res.status(400).send();
+  });
+});
+
 app.listen(process.env.PORT, ()=>{
   console.log(`Started on port ${process.env.PORT}`);
 });
