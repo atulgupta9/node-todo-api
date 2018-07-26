3fddba81c8b746492d3b6010d3230093af1c4b6d
Atul Gupta (UST <U65980@ust-global.com>
2018-07-26 16:43:49 +0530

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,13 @@
 var env = process.env.NODE_ENV || "development";
 console.log("env *****",env);
-
-if(env==="production"){
+if(env === "production"){
   process.env.MONGODB_URI="mongodb://test:test123@ds145981.mlab.com:45981/to-do-api";
-}else if(env === "development"){
-  process.env.PORT =3000;
-  process.env.MONGODB_URI="mongodb://localhost:27017/TodoApp";
-} else if(env === "test"){
-  process.env.PORT =3000;
-  process.env.MONGODB_URI="mongodb://127.0.0.1:27017/TodoAppTest";
 }
-console.log("MONGODB_URI *****",process.env.MONGODB_URI);
+else if(env === "development" || env ==="test"){
+  var config = require("./config.json");
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key]= envConfig[key];
+  });
+}
